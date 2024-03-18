e40ca4d4807ce065f1e2a852dd806d33ad53be3b
Woondu <73076434+MFDOOM-ab@users.noreply.github.com>
2024-03-17 23:07:06 -0400

    Added DB

--- server.js
+++ server.js
@@ -2,23 +2,24 @@ const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const bodyParser = require('body-parser');
 
+const db = new sqlite3.Database('./TimeSync.db', sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    console.log('Connected to the TimeSync SQLite database.');
+});
+
 const app = express();
 const port = 3000;
 app.use(bodyParser.json());
 app.use(express.static('.'));
-
-const db = new sqlite3.Database(':memory:');
-
-db.serialize(function() {
-  db.run("CREATE TABLE users (id INT, username TEXT)");
-  db.run("CREATE TABLE meetings (id INT, title TEXT, user_id INT, FOREIGN KEY(user_id) REFERENCES users(id))");
-});
 
 app.get('/', (req, res) => {
   res.redirect('/user.html');
 });
 
 // Add any new endpoints here
+//Notes: Add CRUD functions
 
 //Add New Users
 app.post('/user', (req, res) => {
