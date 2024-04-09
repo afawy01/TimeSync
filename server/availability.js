4c67ebb0533b9ecfac4e02ac09932b124d7ef189
Lorry <lorryhoang@gmail.com>
2024-04-09 13:22:37 -0400

    availability polling wip

--- server/availability.js
+++ server/availability.js
@@ -13,16 +13,87 @@ let dates = [];
 
 function addDate() {
 	let date = document.getElementById('addDate').value;
-	dates.push(date);
+	if (date){
+		dates.push(date);
+	}
 	document.getElementById('addDate').value = "";
-	displayDates();
+    displayDates();
 }
 
 function displayDates() {
     let list = document.querySelector('.showDates');
+    list.innerHTML="";
     for (i=0; i<dates.length; i++){
         let li = document.createElement('li');
-        li.innerText = dates[i];
+        li.innerText = new Date(dates[i]).toLocaleString();
         list.appendChild(li);
     }
+}
+
+function createPoll() {
+    class Poll {
+        constructor(root, title) {
+            this.root = root;
+            this.selected = sessionStorage.getItem("poll-selected");
+            this.endpoint = "http://localhost:3000/availability.html";
+
+            this.root.insertAdjacentHTML("afterbegin", `
+                <div class="poll__title">${ title }</div>
+            `);
+
+            this._refresh();
+        }
+
+        async _refresh() {
+            const response = await fetch(this.endpoint);
+            const data = await response.json();
+
+            this.root.querySelectorAll(".poll__option").forEach(option => {
+                option.remove();
+            });
+
+            for (const option of dates) {
+                const template = document.createElement("template");
+                const fragment = template.content;
+
+                template.innerHTML = `
+                    <div class="poll__option ${ this.selected == option.label ? "poll__option--selected": "" }">
+                        <div class="poll__option-fill"></div>
+                        <div class="poll__option-info">
+                            <span class="poll__label">${ option.label }</span>
+                            <span class="poll__percentage">${ option.percentage }%</span>
+                        </div>
+                    </div>
+                `;
+
+                if (!this.selected) {
+                    fragment.querySelector(".poll__option").addEventListener("click", () => {
+                        fetch(this.endpoint, {
+                            method: "post",
+                            body: `add=${ option.label }`,
+                            headers: {
+                                "Content-Type": "application/x-www-form-urlencoded"
+                            }
+                        }).then(() => {
+                            this.selected = option.label;
+
+                            sessionStorage.setItem("poll-selected", option.label);
+
+                            this._refresh();
+                        })
+                    });
+                }
+
+                fragment.querySelector(".poll__option-fill").style.width = `${ option.percentage }%`;
+
+                this.root.appendChild(fragment);
+            }
+        }
+    }
+
+    const p = new Poll(
+        document.querySelector(".poll"),
+        "Choose available meeting times below."
+    );
+
 }
--- server/server.js
+++ server/server.js
@@ -443,6 +443,42 @@ app.post('/joingroup', (req, res) => {
   })
 })
 
+//Polling stuff
+
+// Support POSTing form data with URL encoded
+app.use(express.urlencoded({ extended: true }));
+
+// Enable CORS
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+
+    next();
+});
+
+app.get("/poll", async (req, res) => {
+    let data = JSON.parse(await fs.readFile(dataFile, "utf-8"));
+    const totalVotes = Object.values(data).reduce((total, n) => total += n, 0);
+
+    data = Object.entries(data).map(([label, votes]) => {
+        return {
+            label,
+            percentage: (((100 * votes) / totalVotes) || 0).toFixed(0)
+        }
+    });
+
+    res.json(data);
+});
+
+app.post("/poll", async (req, res) => {
+    const data = JSON.parse(await fs.readFile(dataFile, "utf-8"));
+
+    data[req.body.add]++;
+
+    await fs.writeFile(dataFile, JSON.stringify(data));
+
+    res.end();
+});
+
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
